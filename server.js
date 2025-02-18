bafdc2cca0d1ee4d937e56d4f10e03e42d3460c8
Mark DLC <mark@dlcevents.com>
2025-02-18 14:07:35 +0400

    Removed deprecated MongoDB options

--- server.js
+++ server.js
@@ -17,9 +17,8 @@ if (!process.env.MONGO_URI) {
 }
 
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => console.log("Connected to MongoDB Atlas"))
+});.then(() => console.log("Connected to MongoDB Atlas"))
   .catch(err => console.error("MongoDB Connection Error:", err));
 
 const PinSchema = new mongoose.Schema({
